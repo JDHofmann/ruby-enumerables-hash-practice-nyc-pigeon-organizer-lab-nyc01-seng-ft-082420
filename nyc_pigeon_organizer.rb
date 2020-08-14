a2583d1d12e8a9ffe0aa3fd545189c67567fb92d
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-14 19:19:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,22 @@
 require 'pry'
-
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(k,v), final_array|
     v.each do |k_2, v_2|
@@ -12,9 +29,13 @@ def nyc_pigeon_organizer(data)
         end 
           final_array[i][k].push(k_2)
       end
+      
+      
     #end of v.each   
     end 
+    
   #end of data.each  
   end 
+  # puts pigeon_data[:color]
 end
 binding.pry 
