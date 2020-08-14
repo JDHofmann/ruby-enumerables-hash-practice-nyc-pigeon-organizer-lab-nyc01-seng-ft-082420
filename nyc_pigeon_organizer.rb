8e2f4b1c87e0339fc54997d46eba23b9655b0875
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-14 17:35:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -27,7 +27,7 @@ def nyc_pigeon_organizer(data)
       v_2.map do |i|
         # add each to pigeon_data hash as a key 
         # 
-        pigeon_data[i] = ""
+        pigeon_data[i] = {}
       end
       
     #end of v.each   
