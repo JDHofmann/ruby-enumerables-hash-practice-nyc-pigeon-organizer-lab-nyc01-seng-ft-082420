e47a7e466beae188e9b859141d9b4068e763bb28
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-14 19:00:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,7 +20,7 @@ pigeon_data = {
 def nyc_pigeon_organizer(data)
   pigeon_data = {}
 
-  data.each_with_object do |k,v|
+  data.each_with_object({}) do |(k,v), a|
     
     v.each do |k_2, v_2|
       
