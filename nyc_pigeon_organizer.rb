afe4b83186eb0b430a985bfe01bb5e44ae080a2f
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-14 18:59:39 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,17 +20,18 @@ pigeon_data = {
 def nyc_pigeon_organizer(data)
   pigeon_data = {}
 
-  data.each do |k,v|
+  data.each_with_object do |k,v|
     
     v.each do |k_2, v_2|
       
       v_2.map do |i|
+        
         # add each to pigeon_data hash as a key 
         # pigeon_data[i] = "#{k}"
         #puts "#{k}:#{k_2}:#{i}" 
         # pigeon_data[k_2] = "#{i}"
+        
           pigeon_data[i] = "#{k}"
-          # pigeon_data[i,{"#{k}"}] = "#{k_2}"
 
       end
       
